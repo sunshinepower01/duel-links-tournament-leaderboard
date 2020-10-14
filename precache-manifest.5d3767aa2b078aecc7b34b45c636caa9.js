aa960d3fda26237b591edabaf0879f951ffb86e6
Kevin Anderson <sunshinepower01@gmail.com>
2020-10-14 21:11:13 +0700

    Updates

---
+++ precache-manifest.5d3767aa2b078aecc7b34b45c636caa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc3e5e348453ee6f99a4ba85c85e091",
+    "url": "/duel-links-tournament-leaderboard/index.html"
+  },
+  {
+    "revision": "0d01469a174b9cddac48",
+    "url": "/duel-links-tournament-leaderboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e8c5d8a5f575c259f8d",
+    "url": "/duel-links-tournament-leaderboard/static/js/2.66a9bc22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/duel-links-tournament-leaderboard/static/js/2.66a9bc22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d01469a174b9cddac48",
+    "url": "/duel-links-tournament-leaderboard/static/js/main.c9f1b2b0.chunk.js"
+  },
+  {
+    "revision": "218fca10509ae96b612b",
+    "url": "/duel-links-tournament-leaderboard/static/js/runtime-main.0e95cb30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/duel-links-tournament-leaderboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
